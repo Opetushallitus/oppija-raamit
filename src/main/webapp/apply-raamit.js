807eacc01cfd1de8c41d5d3d6c8ec99ecd9b694c
Juha Paananen <juha.paananen@gmail.com>
2014-08-01 14:55:42 +0300

    translations in swedish, english. correct wordpress root for en

--- src/main/webapp/apply-raamit.js
+++ src/main/webapp/apply-raamit.js
@@ -2,14 +2,13 @@
 
   var rootDirectory = getScriptDirectory();
   var raamitDirectory = rootDirectory + "oppija-raamit"
-  var wordPressRoot = (rootDirectory.indexOf("localhost") > 0) ? "https://testi.opintopolku.fi/wp/" : "/wp/"
+  var wordPressHost = (rootDirectory.indexOf("localhost") > 0) ? "https://testi.opintopolku.fi" : ""
 
   initJQuery(function() {
     initJQueryCookie(function() {
-      var lang = jQuery.cookie("i18next") || "fi"
-      var naviUrl = wordPressRoot + lang + "/api/nav/json_nav/"
-      var naviAjax = $.ajax(naviUrl)
       initI18n(function() {
+        var naviUrl = wordPressHost + i18n.t("wordpressRoot") + "/api/nav/json_nav/"
+        var naviAjax = $.ajax(naviUrl)
         loadScript(window.navigationMenubar, rootDirectory + "/js/navigation.js", function() {
           $.ajax(raamitDirectory + "/oppija-raamit.html").done(function(template) {
             applyRaamit(template)
@@ -100,6 +99,7 @@
       var dictionary = {
         fi: {
           translation: {
+            wordpressRoot: "/wp/fi",
             homeLink: {
               title: "Siirry etusivulle",
               image: "opintopolku_large-fi.png"
@@ -136,33 +136,34 @@
         },
         sv: {
           translation: {
+            wordpressRoot: "/wp/sv",
             homeLink: {
               title: "Gå till framsida",
-              image: "opintopolku_large-sv.png" // TODO: tästä eteenpäin kääntämättä
+              image: "opintopolku_large-sv.png"
             },
             checklist:{
-              title: "Muistilista"
+              title: "Minneslista"
             },
             registerDescription: {
-              title: "Rekisteriseloste",
-              link: "/wp/fi/rekisteriseloste/"
+              title: "Registerbeskrivning",
+              link: "/wp/sv/registerbeskrivning/"
             },
             siteMap: {
-              title: "Hakemisto",
-              link: "/fi/hakemisto/oppilaitokset/A"
+              title: "Index",
+              link: "/sv/hakemisto/oppilaitokset/A"
             },
             opetushallitus: {
-              title: "Opetushallitus",
-              image: "OPH_logo-fi.png",
-              link: "http://www.oph.fi/etusivu"
+              title: "Utbildningsstyrelsen",
+              image: "OPH_logo-sv.png",
+              link: "http://www.oph.fi/startsidan"
             },
             opetusministerio: {
-              title: "Opetusministeriö",
-              image: "OKM_logo-fi.png",
-              link: "http://www.minedu.fi/OPM/"
+              title: "Undervisnings- och kulturministeriet",
+              image: "OKM_logo-sv.png",
+              link: "http://www.minedu.fi/OPM/?lang=sv"
             },
-            footerNote: "Koulutuksen järjestäjät ja korkeakoulut ylläpitävät tietoja koulutuksistaan Opintopolussa. Tietojen oikeellisuuden voit tarkistaa kyseisestä oppilaitoksesta tai korkeakoulusta.",
-            copyright: "Copyright © 2014 Opetushallitus",
+            footerNote: "Utbildningsanordnarna och högskolorna uppdaterar själva uppgifterna om sina utbildningar i Studieinfo. Du kan kontrollera att uppgifterna är riktiga av läroanstalten eller högskolan.",
+            copyright: "Copyright © 2014 Utbildningsstyrelsen",
             languages: {
               fi: "Suomeksi",
               sv: "På svenska",
@@ -172,33 +173,34 @@
         },
         en: {
           translation: {
+            wordpressRoot: "/wp2/en",
             homeLink: {
-              title: "Siirry etusivulle",
-              image: "opintopolku_large-fi.png"
+              title: "Go to frontpage",
+              image: "opintopolku_large-en.png"
             },
             checklist:{
-              title: "Muistilista"
+              title: "My shortlist"
             },
             registerDescription: {
-              title: "Rekisteriseloste",
-              link: "/wp/fi/rekisteriseloste/"
+              title: "Register description",
+              link: "/wp2/en/register"
             },
             siteMap: {
-              title: "Hakemisto",
-              link: "/fi/hakemisto/oppilaitokset/A"
+              title: "Educational institution index",
+              link: "/en/hakemisto/oppilaitokset/"
             },
             opetushallitus: {
-              title: "Opetushallitus",
-              image: "OPH_logo-fi.png",
-              link: "http://www.oph.fi/etusivu"
+              title: "Finnish National Board of Education",
+              image: "OPH_logo-en.png",
+              link: "http://www.oph.fi/english"
             },
             opetusministerio: {
-              title: "Opetusministeriö",
-              image: "OKM_logo-fi.png",
-              link: "http://www.minedu.fi/OPM/"
+              title: "Ministry of Education and Culture",
+              image: "OKM_logo-en.png",
+              link: "http://www.minedu.fi/OPM/?lang=en"
             },
-            footerNote: "Koulutuksen järjestäjät ja korkeakoulut ylläpitävät tietoja koulutuksistaan Opintopolussa. Tietojen oikeellisuuden voit tarkistaa kyseisestä oppilaitoksesta tai korkeakoulusta.",
-            copyright: "Copyright © 2014 Opetushallitus",
+            footerNote: "The education providers and higher education institutions maintain their study programme information on Studyinfo. You can check the validity of the information directly from the educational institution or the higher education institution.",
+            copyright: "Copyright @ 2014 Finnish National Board of Education",
             languages: {
               fi: "Suomeksi",
               sv: "På svenska",
@@ -208,7 +210,10 @@
         }
       }
       i18n.init({ resStore: dictionary });
-      $.i18n.init({ lng: "fi", resStore: dictionary }); // TODO: lang
+      $.i18n.init({
+        lng: jQuery.cookie("i18next") || "fi",
+        resStore: dictionary
+      })
       window.changeLanguage = function(language) {
         jQuery.cookie("i18next", language); document.location.reload()
       }
