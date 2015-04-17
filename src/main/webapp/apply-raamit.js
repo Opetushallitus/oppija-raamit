cc813c1b1b5c5698309d9e6e2a782642f4066737
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2015-04-17 14:09:28 +0300

    BUG-290: Jos kieli on undefined (kuten getNaviPath ja getFooterLinksPath kutsuissa) päätellään kieli getInitLangilla.

--- src/main/webapp/apply-raamit.js
+++ src/main/webapp/apply-raamit.js
@@ -136,6 +136,9 @@
   
   //Check due to ajax fail in IE9
   function checkForLanguageMatchingWp(wp, lang) {
+      if(!lang){
+          lang = getInitLang();
+      }
       if (lang == "en" && wp == "/wp/") {
           return "/wp2/";
       } else if (lang != "en" && wp == "/wp2/en/") {
