0746f23f824a3e452a49e865bbd32b603e7dc769
Tatu Honkanen <tatu.honkanen@cybercom.com>
2018-12-04 13:25:05 +0200

    OY-271 : Browser update not showing always.

--- src/main.js
+++ src/main.js
@@ -37,7 +37,6 @@ import './styles/main.css';
     browserUpdate({required:{e:-4,f:-3,o:-3,s:-1,c:-3},
       insecure:true,
       unsupported:true,
-      test:true,
       text:buText,
       no_permanent_hide:true,
       api:2018.12 });
