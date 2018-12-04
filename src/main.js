bd4eb92f8eda12e87ff62899bfb980e5274270f2
Tatu Honkanen <tatu.honkanen@cybercom.com>
2018-12-04 15:00:38 +0200

    OY-271 : Cleanup

--- src/main.js
+++ src/main.js
@@ -27,21 +27,30 @@ import './styles/main.css';
 
     checkAcceptCookie();
 
+    var updateText;
     if (getLanguage() === 'fi') {
-      var buText = 'Selaimesi {brow_name} on vanhentunut. Päivitä selaimesi turvallisempaan, nopeampaan ja helppokäyttöisempään. <a{up_but}>Päivitä selain</a><a{ignore_but}>Hylkää</a>'
+      updateText = 'Selaimesi {brow_name} on vanhentunut. Päivitä selaimesi turvallisempaan, nopeampaan ja helppokäyttöisempään. <a{up_but}>Päivitä selain</a><a{ignore_but}>Hylkää</a>'
     } else if (getLanguage() === 'sv') {
-      var buText = 'Din webbläsare {brow_name} är föråldrad. Uppdatera din webbläsare för mer säkerhet, snabbhet och den bästa upplevelsen. <a{up_but}>Uppdatera webbläsaren</a><a{ignore_but}>Ignorera</a>'
+      updateText = 'Din webbläsare {brow_name} är föråldrad. Uppdatera din webbläsare för mer säkerhet, snabbhet och den bästa upplevelsen. <a{up_but}>Uppdatera webbläsaren</a><a{ignore_but}>Ignorera</a>'
     } else {
-      var buText = 'Your web browser {brow_name}, is out of date. Update your browser for more security, speed and the best experience. <a{up_but}>Update browser</a><a{ignore_but}>Ignore</a>'
+      updateText = 'Your web browser {brow_name}, is out of date. Update your browser for more security, speed and the best experience. <a{up_but}>Update browser</a><a{ignore_but}>Ignore</a>'
     }
-    browserUpdate({required:{e:-4,f:-3,o:-3,s:-1,c:-3},
+    browserUpdate({
+      required:{
+        e:-4,
+        f:-3,
+        o:-3,
+        s:-1,
+        c:-3
+      },
       insecure:true,
       unsupported:true,
-      text:buText,
+      text:updateText,
       no_permanent_hide:true,
-      api:2018.12 });
+      api:2018.12
+    });
 
-  });
+    });
 
 })();
 
