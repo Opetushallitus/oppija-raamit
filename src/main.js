0f72cd516a5217e3f3ea18e32d27024a44dd6bfd
Tatu Honkanen <tatu.honkanen@cybercom.com>
2018-12-04 12:52:55 +0200

    OY-271 : Added browser update script for testing - now showing always for testing purposes.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {updateDom, toggleMobileMenu, toggleOverflowMenu} from './js/dom';
 import {checkAcceptCookie, setAcceptCookie} from './js/cookie';
 import {getLanguage, changeLanguage} from './js/language';
 import {login, logout, getUser} from './js/login';
+import browserUpdate from 'browser-update';
 
 import './styles/main.css';
 
@@ -25,6 +26,22 @@ import './styles/main.css';
     updateDom(lang);
 
     checkAcceptCookie();
+
+    if (getLanguage() === 'fi') {
+      var buText = 'Selaimesi {brow_name} on vanhentunut. Päivitä selaimesi turvallisempaan, nopeampaan ja helppokäyttöisempään. <a{up_but}>Päivitä selain</a><a{ignore_but}>Hylkää</a>'
+    } else if (getLanguage() === 'sv') {
+      var buText = 'Din webbläsare {brow_name} är föråldrad. Uppdatera din webbläsare för mer säkerhet, snabbhet och den bästa upplevelsen. <a{up_but}>Uppdatera webbläsaren</a><a{ignore_but}>Ignorera</a>'
+    } else {
+      var buText = 'Your web browser {brow_name}, is out of date. Update your browser for more security, speed and the best experience. <a{up_but}>Update browser</a><a{ignore_but}>Ignore</a>'
+    }
+    browserUpdate({required:{e:-4,f:-3,o:-3,s:-1,c:-3},
+      insecure:true,
+      unsupported:true,
+      test:true,
+      text:buText,
+      no_permanent_hide:true,
+      api:2018.12 });
+
   });
 
 })();
