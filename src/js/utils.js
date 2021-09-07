d5412de918cb50c028d75868116a712b81f1fb51
August Kilponen <august.kilponen@gmail.com>
2021-09-07 21:43:10 +0300

    Poistettu muun kielisten domainien linkkien tuki

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,19 +4,3 @@ export function parseHtml(html) {
   template.innerHTML = html;
   return template.content.cloneNode(true);
 }
-
-export function createDomain(lang) {
-  const origin = document.location.origin;
-  const domains = {
-    fi: 'opintopolku.fi',
-    sv: 'studieinfo.fi',
-    en: 'studyinfo.fi'
-  };
-
-  if (origin.includes('https://')) {
-    const domainPrefix = origin.replace(/opintopolku.fi|studyinfo.fi|studieinfo.fi/g, '');
-    const domainSuffix = domains[lang];
-    return domainPrefix + domainSuffix;
-  }
-  return '';
-}
